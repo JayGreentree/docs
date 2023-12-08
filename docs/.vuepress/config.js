a47c06ec2de66597f34f4d69be73ed11cf398227
Jay Greentree <JayGreentree@users.noreply.github.com>
2023-12-08 10:01:44 -0500

    Delete docs/.vuepress/config.js

--- docs/.vuepress/config.js
+++
@@ -1,8 +0,0 @@
-import { defineUserConfig } from 'vuepress'
-
-export default defineUserConfig({
-  lang: 'en-US',
-  title: 'Greentree Consulting Documentation',
-  description: 'website Documentationw',
-  dest: './docs/'
-})
